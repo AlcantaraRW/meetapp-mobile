87848c89bbc90b18df6011248202f80e8e255a98
Rafael Alcantara <rafael_william_@hotmail.com>
2019-09-13 21:09:53 -0300

    Initial.

---
+++ src/pages/Meetapps/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Meetapps() {
+  return <Text>Meetapps</Text>;
+}
